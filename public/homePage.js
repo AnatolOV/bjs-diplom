610eba972ee1b46e8a4a4c4af216b184e49e5602
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-12-09 00:05:25 +0300

    342

--- public/homePage.js
+++ public/homePage.js
@@ -28,29 +28,20 @@ function getCurrency () {
 
 setInterval(getCurrency, 1000);
 
-/*Создайте объект типа MoneyManager
-
-Реализуйте пополнение баланса:
-Запишите в свойство addMoneyCallback функцию, которая будет выполнять запрос.
-
-Внутри функции выполните запрос на пополнение баланса (addMoney).
-
-Используйте аргумент функции свойства addMoneyCallback для передачи данных data в запрос - какие данные нужно передать? Их указать в аргументе f(?) .
-После выполнения запроса выполните проверку успешности запроса.
-В случае успешного запроса отобразите в профиле новые данные о пользователе из данных ответа от сервера (showProfile). - (я правильно записал?)
-Также выведите сообщение об успехе или ошибку (причину неудачного действия) пополнении баланса в окне отображения сообщения (setMessage). - что передавать в аргументы setMessage(isSuccess, message)?
-*/
-
 let manageMoney = new MoneyManager();
 
- manageMoney.addMoneyCallback = function f (data) {  
-  ApiConnector.addMoney({ currency, amount }, callback => {
-      if (callback.success == true){
-        ProfileWidget.showProfile(callback.data);
-        MoneyManager.setMessage(isSuccess, message);
-        console.log(3);
- }});
-  }
+ manageMoney.addMoneyCallback = function f({ currency, amount }) {
+   ApiConnector.addMoney({ currency, amount }, (callback) => {
+     if (callback.success == true) {
+       ProfileWidget.showProfile(callback.data);
+       console.log(callback.data)
+       manageMoney.setMessage(callback.success, console.error("Успешно"));//Также выведите сообщение об успехе или 
+       //ошибку (причину неудачного действия) пополнении баланса в окне 
+      //отображения сообщения (setMessage). -??? что передавать в аргументы setMessage(isSuccess, message)?
+      
+     }
+   });
+ };
 
 
 
